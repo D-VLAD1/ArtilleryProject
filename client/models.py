beee92cb4e47ea215d736861279ff3f980204dbd
Vlad Danylyshyn <vladuslavsd@gmail.com>
2025-03-29 18:18:13 +0200

    Add database

--- client/models.py
+++ client/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ArtillerySettings(models.Model):
+    name = models.CharField(max_length=100)
+    max_distance = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Settings'
