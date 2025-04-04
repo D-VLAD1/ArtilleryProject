bd487e1892f7c2a414092e43fb7f82ad3f502988
Ivan Zarytskyi <ivan.zarytskyi1991@gmail.com>
2025-04-04 16:24:19 +0300

    Populate db

--- client/models.py
+++ client/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class ArtillerySettings(models.Model):
     name = models.CharField(max_length=100)
-    max_distance = models.IntegerField()
-    bullet_speed = models.IntegerField()
+    max_distance = models.JSONField(default="{}")
+    bullet_speed = models.IntegerField(default=0)
+    min_angle = models.DecimalField(max_digits=3, decimal_places=1, default=0)
+    max_angle = models.DecimalField(max_digits=3, decimal_places=1, default=90)
 
     class Meta:
         verbose_name_plural = 'Settings'
