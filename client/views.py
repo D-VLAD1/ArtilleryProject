0765d4dbe84603b2703f480b77192832d4131c16
Vlad Danylyshyn <vladuslavsd@gmail.com>
2025-04-08 16:08:00 +0300

    Reorganize file structure

--- client/views.py
+++ client/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(requests):
-    return render(requests, 'client/home.html')
+    return render(requests, 'client/index.html')
+
+
+def map(requests):
+    return render(requests, 'client/map.html')
