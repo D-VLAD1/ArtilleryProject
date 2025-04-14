ca22e3c682ef12d7fa6b7fd4cd3cdbafdca3cdae
Vlad Danylyshyn <vladuslavsd@gmail.com>
2025-04-14 17:53:39 +0300

    Add python communication 2

--- client/views.py
+++ client/views.py
@@ -16,9 +16,9 @@ def compute(requests):
     data = {}
     if requests.method == 'POST':
         post_data = json.loads(requests.body)
-        location, target, weapon = post_data.get('location'), post_data.get('target'), post_data.get('location')
+        location, target, weapon = post_data.get('location'), post_data.get('target'), post_data.get('weapon')
 
-        angle = math.atan()
+        print(location, target, weapon)
 
 
     return JsonResponse({'Error': 'Something went wrong'} if not data else data)
