e5fcbafd447f1a71e3767d0d083912523b3c57c6
Vlad Danylyshyn <vladuslavsd@gmail.com>
2025-04-25 21:18:50 +0300

    Deploy preparation

--- Artillery/settings.py
+++ Artillery/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u7lqr10iaz#+j%)lgmt#r&od4l&e03oqchm#4#lc9qekbuyc-6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
